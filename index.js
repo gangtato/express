221994ea91cf1447f2ad60b9611f0dabb38e376b
Aldi <aldyy1988@gmail.com>
2021-10-20 23:05:08 +0700

    Update index

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const dotenv = require('dotenv');
 
 // const productRouter = require('./src/product/routes');
 // const productRouterV2 = require('./src/product_v2/routes');
-// const productRouterV3 = require('./src/product_v3/routes');
+const productRouterV3 = require('./src/product_v3/routes');
 const productRouterV4 = require('./src/product_v4/routes');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 dotenv.config();
 
 app.use(logger('dev'))
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use('/public', express.static(path.join(__dirname, 'uploads')));
 // app.use('/api/v1/', productRouter);
 // app.use('/api/v2/', productRouterV2);
-// app.use('/api/v3/', productRouterV3);
+app.use('/api/v3/', productRouterV3);
 app.use('/api/v4/', productRouterV4);
 
 app.listen(port, () => console.log(`PORT ${port}`));
