c32975d1b051a7c53d6fb7d45a2b6c1cf20fe9b4
Aldi <aldyy1988@gmail.com>
2021-10-20 22:04:05 +0700

    Update Uploads

--- src/product_v4/routes.js
+++ src/product_v4/routes.js
@@ -6,7 +6,7 @@ const productController = require('./controller');
 router.get('/product', productController.index);
 router.get('/product/:id', productController.view);
 router.post('/product', uploads.single('image'), productController.store);
-router.put('/product/:id', upload.single('image'), productController.update);
+router.put('/product/:id', uploads.single('image'), productController.update);
 router.delete('/product/:id', productController.destroy);
 
 
